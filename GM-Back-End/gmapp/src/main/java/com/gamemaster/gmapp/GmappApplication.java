f90a7a51912447a4cea9fad22368a556e1ccac43
cvSquiggles <jones.steven.alexander@gmail.com>
2026-05-29 22:51:51 -0400

    Initial commit for game master web app project.

---
+++ GM-Back-End/gmapp/src/main/java/com/gamemaster/gmapp/GmappApplication.java
@@ -0,0 +1,13 @@
+package com.gamemaster.gmapp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GmappApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GmappApplication.class, args);
+	}
+
+}
---
+++ GM-Back-End/gmapp/src/main/java/com/gamemaster/gmapp/restController.java
@@ -0,0 +1,14 @@
+package com.gamemaster.gmapp;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class restController {
+
+    @GetMapping("/api/mic")
+    public String mic() {
+        return "check, 1, 2";
+    }
+
+}
