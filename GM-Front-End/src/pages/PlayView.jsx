dbaec6e22238cff279341f2f316e053bb88385c2
cvSquiggles <jones.steven.alexander@gmail.com>
2026-06-05 16:45:44 -0400

    Confirmed session content is accessible when rerouted to the play view.

--- GM-Front-End/src/pages/PlayView.jsx
+++ GM-Front-End/src/pages/PlayView.jsx
@@ -1,8 +1,21 @@
-import React from 'react';
+import React from 'react'
+import { useLocation } from 'react-router-dom'
 
 export function PlayView() {
+    const location = useLocation()
+    const session = location.state?.session
+
     return(
+        <div>
         <h1>Play View</h1>
+        <br/>
+        <br/>
+        <div id="sessionInfoDisplay">
+            <p>Game Code: {session?.sessionCode}</p>
+            <p>Session ID: {session?.id}</p>
+            <p>Session Code: {session?.sessionCode}</p>
+        </div>
+        </div>
     );
 }
 
